d8d753750ff7c4d52f66d8e8b74dfc6c48ade46d
MrLiu <610270981@qq.com>
2018-01-24 16:36:02 +0800

    登录注册

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,12 @@
 </template>
 
 <script>
-export default {
+  import Vue from 'vue'
+  import BootstrapVue from 'bootstrap-vue'
+  Vue.use(BootstrapVue)
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
 
 }
 </script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-container>
+            <b-row>
+                <b-col cols="9">我站九个</b-col>
+                <b-col cols="3">我站三个</b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
